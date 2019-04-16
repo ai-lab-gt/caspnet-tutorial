e73dd72936962e65412ebb7bf2d54761ac46bf46
Sebastian Ibarguen <sebasibarguen@gmail.com>
2019-04-16 11:14:43 -0400

    Make train script and models run successfully. Still need to revise loss functions.

--- model/capsnet.py
+++ model/capsnet.py
@@ -29,22 +29,21 @@ class PrimaryCapsules(nn.Module):
         - Input: (batch, channels, height, width)
         - Output: (batch, n_caps, caps_dims)
     """
-    def __init__(self, conv_params, caps_maps=32, caps_dims=8):
+    def __init__(self, conv_params, caps_maps=32, num_capsules=8):
         super(PrimaryCapsules, self).__init__()
-        self.caps_maps = caps_maps
         # Output of conv2 has 256 (32*8) maps of 6x6.
         # We instead want 32 vectors of 8 dims each.
-        self.n_caps = caps_maps * 6 * 6
-        self.caps_dims = caps_dims
+        self.num_routes = caps_maps * 6 * 6
+        self.num_capsules = num_capsules
 
         self.capsules = nn.ModuleList([
-            nn.Conv2d(**conv_params) for _ in range(self.caps_maps)
+            nn.Conv2d(**conv_params) for _ in range(self.num_capsules)
         ])
 
     def forward(self, x):
         output = [capsule(x) for capsule in self.capsules]
-        output = torch.cat(output)
-        output = output.view(x.size(0), -1, self.caps_dims)
+        output = torch.stack(output, dim=1)
+        output = output.view(x.size(0), self.num_routes, -1)
         return squash(output)
 
 # https://github.com/laubonghaudoi/CapsNet_guide_PyTorch/blob/master/DigitCaps.py
@@ -58,60 +57,92 @@ class DigitCapsule(nn.Module):
     """
     def __init__(self, num_route_nodes, in_channels, out_channels, num_iterations):
         super(DigitCapsule, self).__init__()
-        self.num_digits = 10
+        self.num_capsules = 10
         self.num_route_nodes = num_route_nodes
         self.num_iterations = num_iterations
         # W.shape => [1, 10, 1152, 8, 16]
         # (1) is to be broadcastable with torch.matmul
         self.W = nn.Parameter(torch.randn(1,
                                         self.num_route_nodes,
-                                        self.num_digits,
+                                        self.num_capsules,
                                         out_channels,
                                         in_channels,
                                         ))
 
-    def forward(self, u):
-        u = u[:,:,None,:,None,]
-        u_hat = torch.matmul(self.W, u)
-        u_hat = torch.squeeze(u_hat)
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = torch.stack([x] * self.num_capsules, dim=2).unsqueeze(4)
+
+        W = torch.cat([self.W] * batch_size, dim=0)
+        u_hat = torch.matmul(W, x)
 
         # Routing Algorithm.
         # for all capsule i in layer l and capsule j in layer (l + 1): b_ij ← 0
-        b = Variable(torch.zeros(self.num_route_nodes, self.num_digits))
+        b = Variable(torch.zeros(1, self.num_route_nodes, self.num_capsules, 1))
         # for r iterations do
         for i in range(self.num_iterations):
             # for all capsule i in layer l: c_i ← softmax(b_i)
-            c = F.softmax(b, dim=1).unsqueeze(2).unsqueeze(0)
+            c = F.softmax(b)
+            c = torch.cat([c] * batch_size, dim=0).unsqueeze(4)
             # for all capsule j in layer (l+1): s_j ← SUM_i ( c_ij * u_hat_j|i )
-            s = torch.sum(u_hat * c, dim=1)
+            s = (u_hat * c).sum(dim=1, keepdim=True)
             # for all capsule j in layer (l + 1): v_j ← squash(s_j)
             v = squash(s)
             # for all capsule i in layer l and capsule j in layer (l + 1): b_ij ← b_ij + u_hat_j|i * v_j
-            a = torch.matmul(u_hat.transpose(1, 2), v.unsqueeze(3))
-            b = b + torch.sum(a.squeeze().transpose(1, 2), dim=0)
-        return v
+            if i < self.num_iterations - 1:
+                a = torch.matmul(u_hat.transpose(3, 4), torch.cat([v] * self.num_route_nodes, dim=1))
+                b = b + a.squeeze(4).mean(dim=0, keepdim=True)
 
-class CapsNet(nn.Module):
-    def __init__(self, conv1_params, conv2_params):
-        super(CapsNet, self).__init__()
-        self.conv = nn.Conv2d(**conv1_params)
-        self.primary_capsules = PrimaryCapsules(conv2_params, caps_dims=8)
-        self.digit_capsules = DigitCapsule(num_route_nodes=32*6*6, in_channels=8, out_channels=16, num_iterations=3)
+        return v.squeeze(1)
 
-        self.decoder = nn.Sequential(
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        
+        self.reconstraction_layers = nn.Sequential(
             nn.Linear(16 * 10, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 1024),
             nn.ReLU(inplace=True),
             nn.Linear(1024, 784),
             nn.Sigmoid()
-        ) 
+        )
+        
+    def forward(self, x, data):
+        classes = torch.sqrt((x ** 2).sum(2))
+        classes = F.softmax(classes)
+        
+        _, max_length_indices = classes.max(dim=1)
+        masked = Variable(torch.sparse.torch.eye(10))
+        masked = masked.index_select(dim=0, index=max_length_indices.squeeze(1).data)
+        
+        reconstructions = self.reconstraction_layers((x * masked[:, :, None, None]).view(x.size(0), -1))
+        reconstructions = reconstructions.view(-1, 1, 28, 28)
+        
+        return reconstructions, masked
+
+class CapsNet(nn.Module):
+    def __init__(self, conv1_params, conv2_params):
+        super(CapsNet, self).__init__()
+        self.conv = nn.Conv2d(**conv1_params)
+        self.primary_capsules = PrimaryCapsules(conv2_params, num_capsules=8)
+        self.digit_capsules = DigitCapsule(num_route_nodes=32*6*6, in_channels=8, out_channels=16, num_iterations=3)
+        self.decoder = Decoder()
+        # self.decoder = nn.Sequential(
+        #     nn.Linear(16 * 10, 512),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(512, 1024),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(1024, 784),
+        #     nn.Sigmoid()
+        # ) 
 
     def forward(self, x):
         x = F.relu(self.conv(x))
         u = self.primary_capsules(x)
         v = self.digit_capsules(u)
         print('V size', v.size())
-        reconstruction = self.decoder(v)
+        reconstruction, masked = self.decoder(v, x)
         print('reconstruction size', reconstruction.size())
         return v, reconstruction
